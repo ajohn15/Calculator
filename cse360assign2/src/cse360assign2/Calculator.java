49fe131ba09d04c5a792f8a24e09c7a910b6e473
sajimarkose <sajimarkose@192.168.0.8>
2019-02-24 18:44:20 -0700

    Completed the getHistory method

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -5,7 +5,7 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
-	
+	private String history = "0";
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
@@ -21,35 +21,42 @@ public class Calculator {
 	
 	/**
 	 * Updates the private variable total by adding the value of the parameter.
-	 *  
+	 * Updates history by appending '+' sign and the parameter to the variable history
+	 * 
 	 * @param integer value to add to total
 	 */
 	public void add (int value) {
 		total = total + value;
+		history = history + " + " + value;
 	}
 	
 	/**
 	 * Updates the private variable total by subtracting the value of the parameter.
+	 * Updates history by appending '-' sign and the parameter to the variable history 
 	 *  
 	 * @param integer value to subtract from the total
 	 */
 	public void subtract (int value) {
 		total = total - value;
+		history = history + " - " + value;
 	}
 	
 	/**
 	 * Updates the private variable total by multiplying value of the 
-	 * parameter with the total variable.
-	 *  
+	 * parameter with the total variable. Updates history by appending '*' sign 
+	 * and the parameter to the variable history
+	 * 
 	 * @param integer value to multiply with total
 	 */
 	public void multiply (int value) {
 		total = total*value;
+		history = history + " * " + value;
 	}
 	
 	/**
 	 * Updates the private variable total by dividing the total by 
-	 * the value of the parameter. 
+	 * the value of the parameter. Updates history by appending '/' sign 
+	 * and the parameter to the variable history.
 	 *  
 	 * @param integer value by which the total is divided
 	 */
@@ -61,17 +68,18 @@ public class Calculator {
 		else
 		{
 			total = total/value;
+			history = history + " / " + value;
 		}
 	}
 	
 	/**
-	 * Returns the history of the changes made to the total variable
-	 * after each method is called
-	 *  
-	 * @return String message of all previous calculations
+	 * Returns the history of the arithmetic done to the initial total value
+	 * in the form of +, -, * or / in between the integer parameters
+	 * 
+	 * @return String message of all previous arithmetic
 	 */
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
 
