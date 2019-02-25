73d53d922a489182fd07a7edc86f5108c83540fa
abeljohn <abelsaji@yahoo.com>
2019-02-24 18:03:33 -0700

    Initial commit of Calculator

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,71 @@
+
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total amount after calculations as an integer.
+	 *  
+	 * @return integer total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Updates the private variable total by adding the value of the parameter.
+	 *  
+	 * @param integer value to add to total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Updates the private variable total by subtracting the value of the parameter.
+	 *  
+	 * @param integer value to subtract from the total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Updates the private variable total by multiplying value of the 
+	 * parameter with the total variable.
+	 *  
+	 * @param integer value to multiply with total
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Updates the private variable total by dividing the total by 
+	 * the value of the parameter. 
+	 *  
+	 * @param integer value by which the total is divided
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns the history of the changes made to the total variable
+	 * after each method is called
+	 *  
+	 * @return String message of all previous calculations
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
+
+
